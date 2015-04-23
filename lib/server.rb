1c44e6ca830c5c7458a6e9c4850a2c56ea546d48
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-23 21:33:01 +0100

    Submitting a new link

--- lib/server.rb
+++ lib/server.rb
@@ -7,19 +7,20 @@ class BookmarkManager3 < Sinatra::Base
   get '/' do
   @links = Link2.all
   erb :index
-end
-end
+  end
 
-# we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
+  post '/links' do
+    url = params['url']
+    title = params['title']
+    Link2.create(url: url, title: title)
+    redirect to('/')
+  end
+end
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager3_#{env}")
 
-require './lib/link2' # this needs to be done after datamapper is initialised
-
-# After declaring your models, you should finalise them
+require './lib/link2'
 
 DataMapper.finalize
-
-# However, the database tables don't exist yet. Let's tell datamapper to create them
 
 DataMapper.auto_upgrade!
