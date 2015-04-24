b8416e92e32e932831abd84a7211e30da144c9cb
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-24 10:20:15 +0100

    Handling input errors

--- lib/server.rb
+++ lib/server.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'data_mapper'
+require 'rack-flash'
 require './lib/link2'
 require './lib/tag2'
 require './lib/user2'
@@ -16,6 +17,8 @@ helpers do
 
 end
   enable :sessions
+  use Rack::Flash
+  use Rack::MethodOverride
   set :session_secret, 'super secret'
 
   get '/' do
@@ -39,23 +42,24 @@ end
     erb :index
   end
 
-  get '/users/new' do
-    # note the view is in views/users/new.erb
-    # we need the quotes because otherwise
-    # ruby would divide the symbol :users by the
-    # variable new (which makes no sense)
-    erb :'users/new'
-  end
-
-  post '/users' do
-    user = User2.create(email: params[:email],
-                       password: params[:password],
-                       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/')
-  end
+get '/users/new' do
+  @user = User2.new
+  erb :'users/new'
 end
 
+post '/users' do
+  @user = User2.new(email: params[:email],
+                  password: params[:password],
+                  password_confirmation: params[:password_confirmation])
+if @user.save
+    session[:user_id] = @user.id
+    redirect to('/')
+  else
+    flash[:notice] = 'Sorry, your passwords do not match'
+    erb :'users/new'
+  end
+end
+end
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager3_#{env}")
 
 DataMapper.finalize
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -17,8 +17,10 @@ feature 'User signs up' do
     expect(User2.first.email).to eq('alice@example.com')
   end
 
-  scenario 'with a password that does not match' do
+scenario 'with a password that does not match' do
     expect { sign_up('a@a.com', 'pass', 'wrong') }.to change(User2, :count).by(0)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Sorry, your passwords do not match')
   end
 
   def sign_up(email = 'alice@example.com',
