08467def46d47f5f9953463bdf863441ca958edf
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-24 11:33:29 +0100

    Signing in

--- lib/server.rb
+++ lib/server.rb
@@ -57,6 +57,22 @@ else
   erb :'users/new'
 end
 end
+
+get '/sessions/new' do
+  erb :'sessions/new'
+end
+
+post '/sessions' do
+  email, password = params[:email], params[:password]
+  user = User2.authenticate(email, password)
+  if user
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    flash[:errors] = ['The email or password is incorrect']
+    erb :'sessions/new'
+  end
+end
 end
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager3_#{env}")
 
